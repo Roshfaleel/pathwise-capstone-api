a57388772ba213d2553e1613d704eba585b02884
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-20 13:19:20 -0500

    add migration files and create the 3 tables

---
+++ migrations/20241120175459_create_achievements_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('achievements', function(table){
+    table.increments('achievement_id').primary();
+    table.integer('user_id').unsigned().notNullable();
+    table.string('achievement_name').notNullable();
+    table.text('description');
+    table.date('date');
+    table.string('type');
+
+    table.foreign('user_id').references('users.user_id').onDelete('CASCADE');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('achievements');
+};
