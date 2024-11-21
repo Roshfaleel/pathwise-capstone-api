16c4877942fab1b1c21e9dcf3ce1e290e9b57804
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-21 13:39:05 -0500

    add the index.js file

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import "dotenv/config";
+import initKnex from "knex";
+import configuration from "./knexfile.js";
+import cors from "cors";
+
+const knex = initKnex(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const { PORT, DB_HOST } = process.env;
+
+app.get("/", (_req, res) => {
+  res.json({ message: "Welcome! to Pathwise DB" });
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(knex.client.config);
+  console.log(`Listening at http://${DB_HOST}:${PORT}`);
+});
